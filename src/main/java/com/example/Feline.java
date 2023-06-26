e9f028ad95feda066ad1d2c411c46d3146949e7a
AndrewStorozhev <94290160+AndrewStorozhev@users.noreply.github.com>
2023-06-26 18:35:54 +0400

    Revert " Sprint_6"

--- src/main/java/com/example/Feline.java
+++ src/main/java/com/example/Feline.java
@@ -11,7 +11,6 @@ public class Feline extends Animal implements Predator {
 
     @Override
     public String getFamily() {
-
         return "Кошачьи";
     }
 
--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -3,20 +3,20 @@ package com.example;
 import java.util.List;
 
 public class Lion {
-    boolean hasMane;
-    private final Feline feline;
 
-    public Lion(String sex, Feline feline) throws Exception {
-        this.feline = feline;
+    boolean hasMane;
+
+    public Lion(String sex) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
     }
 
+    Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
@@ -29,5 +29,4 @@ public class Lion {
     public List<String> getFood() throws Exception {
         return feline.getFood("Хищник");
     }
-
 }
