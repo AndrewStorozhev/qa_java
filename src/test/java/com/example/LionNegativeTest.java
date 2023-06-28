7e6dabbe3b7184e3b44afdfc84f89c27ae0d774d
Andrey Storozhev <av.storozhev@mail.ru>
2023-06-28 19:25:34 +0400

    num_6.3

--- src/test/java/com/example/LionNegativeTest.java
+++ src/test/java/com/example/LionNegativeTest.java
@@ -8,6 +8,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionNegativeTest {
@@ -15,16 +16,16 @@ public class LionNegativeTest {
     @Mock
     Feline feline;
 
-    
-    @Test
-    public void negativeTestDoesHaveManeLion() {
-        try {
-            Lion lion = new Lion("Другое", feline);
 
-        } catch (Exception exception) {
-            Exception exceptions = new Exception("Используйте допустимые значения пола животного - самец или самка");
-            assertEquals(exception.toString(), exceptions.toString());
+
+        @Test
+        public void negativeTestDoesHaveManeLion() {
+            String exceptionMessage = "Используйте допустимые значения пола животного - самец или самка";
+            Exception exception = assertThrows(Exception.class, () -> new Lion("Другое", feline));
+            assertEquals(exceptionMessage, exception.getMessage());
         }
-    }
+
 
 }
+
+
